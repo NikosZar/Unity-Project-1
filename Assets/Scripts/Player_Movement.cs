15041cdabae2c33cd1669b63bc771aea1826c0f2
NikosZar <nikosz.mykonos@gmail.com>
2021-11-08 23:41:03 -0800

    Update with PlayerMovement Script
    Updated with player ovement script allowing player to move Right with D Key.

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private float speed;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.D))
+        {
+            speed = 0.2f;
+            transform.Translate(1 * speed, body.velocity.y, 0f);
+        }
+    }
+}
